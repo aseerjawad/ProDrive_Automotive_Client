588f9647fb0b220454eb020e6c9f1d9d100c9f0b
aseerjawad <enanic86@gmail.com>
2023-11-14 19:22:46 +0600

    designed our product

--- src/Components/OurProducts/OurProducts.jsx
+++ src/Components/OurProducts/OurProducts.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import useProducts from "../LoadedData/useProducts";
 import OurProduct from "./OurProduct";
+import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
 
 
 
 const OurProducts = () => {
+    const [tabIndex, setTabIndex] = useState(0);
     const products = useProducts();
-    console.log(products);
+    const Lubricants = products.filter((items) => items.type === 'Lubricant')
+    const Brake = products.filter((items) => items.type === 'Brake')
+    const Horn = products.filter((items) => items.type === 'Horn')
+    const Tyre = products.filter((items) => items.type === 'Tyre')
+    const Battery = products.filter((items) => items.type === 'Battery')
+
 
 
     return (
@@ -14,13 +22,47 @@ const OurProducts = () => {
                 <div className=''></div>
                 <h1 className='absolute top-[50%] left-[50%] text-2xl'>Our Products</h1>
             </div>
-            <div className="grid grid-cols-3 max-w-7xl m-auto gap-10">
-                {
-                    products.map(product => <OurProduct key={product.product_name} element={product}></OurProduct>)
-                }
+            <div >
+                <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+                    <TabList className="tabs tabs-boxed">
+                        <Tab className="tab">Lubricants</Tab>
+                        <Tab className="tab">Brake</Tab>
+                        <Tab className="tab">Horn</Tab>
+                        <Tab className="tab">Tyre</Tab>
+                        <Tab className="tab">Battery</Tab>
+                    </TabList>
+                    <TabPanel className="grid grid-cols-3 max-w-7xl m-auto gap-10">
+                        {
+                            Lubricants.map(product => <OurProduct key={product.product_name} element={product}></OurProduct>)
+                        }
+                    </TabPanel>
+                    <TabPanel className="grid grid-cols-3 max-w-7xl m-auto gap-10">
+                        {
+                            Brake.map(product => <OurProduct key={product.product_name} element={product}></OurProduct>)
+                        }
+                    </TabPanel>
+                    <TabPanel className="grid grid-cols-3 max-w-7xl m-auto gap-10">
+                        {
+                            Horn.map(product => <OurProduct key={product.product_name} element={product}></OurProduct>)
+                        }
+                    </TabPanel>
+                    <TabPanel className="grid grid-cols-3 max-w-7xl m-auto gap-10">
+                        {
+                            Tyre.map(product => <OurProduct key={product.product_name} element={product}></OurProduct>)
+                        }
+                    </TabPanel>
+                    <TabPanel className="grid grid-cols-3 max-w-7xl m-auto gap-10">
+                        {
+                            Battery.map(product => <OurProduct key={product.product_name} element={product}></OurProduct>)
+                        }
+                    </TabPanel>
+                </Tabs>
             </div>
         </div>
     );
 };
 
 export default OurProducts;
+
+
+
